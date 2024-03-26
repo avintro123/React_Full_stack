314758eeee71aa35506fdcd8226a5333a42de723
avintro <119504876+avintro@users.noreply.github.com>
2024-03-26 17:01:59 +0530

    Added Hero section

--- chopal/src/components/Hero.js
+++ chopal/src/components/Hero.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import stadium from './images/maxresdefault.jpg'
+// import './Hero.css';
 
 export default function Hero() {
-    return (
-        <div>
 
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaded(true);
+        }, 500);
+    }, []);
+
+
+    return (
+
+        <div className='mt-8'>
+            <div className={`relative w-full h-screen bg-cover bg-center bg-no-repeat ${loaded ? 'opacity-60 transition-opacity duration-1000 ease-in' : 'opacity-0'}`} style={{ backgroundImage: `url(${stadium})` }}>
+            </div>
+            <h1 className='name'>WELCOME TO CHOPAL CRICKET ASSOCIATION OFFICIAL SITE</h1>
+            <div>
+            </div>
         </div>
     )
 }
