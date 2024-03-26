9e0e3864c484483a135311197aa27ba4e65dbb82
avintro <119504876+avintro@users.noreply.github.com>
2024-03-26 18:55:29 +0530

    liitle more adjusments

--- chopal/src/components/Hero.js
+++ chopal/src/components/Hero.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import stadium from './images/maxresdefault.jpg'
-// import './Hero.css';
+import React, { useEffect, useState } from 'react';
+import stadium from './images/maxresdefault.jpg';
+// import Typed from "react-typed";
 
 export default function Hero() {
-
     const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
@@ -12,15 +11,21 @@ export default function Hero() {
         }, 500);
     }, []);
 
-
     return (
-
-        <div className='mt-8'>
-            <div className={`relative w-full h-screen bg-cover bg-center bg-no-repeat ${loaded ? 'opacity-60 transition-opacity duration-1000 ease-in' : 'opacity-0'}`} style={{ backgroundImage: `url(${stadium})` }}>
+        <div className='mt-8 relative'>
+            <div className={`relative w-full h-screen bg-cover bg-center bg-no-repeat ${loaded ? 'opacity-100 transition-opacity duration-1000 ease-in' : 'opacity-0'}`} style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url(${stadium})` }}>
+                <h1 className='w-full h-screen flex justify-center items-center text-center text-4xl font-bold text-[#faebd7] z-12 relative top-1/3'>WELCOME TO CHOPAL CRICKET ASSOCIATION OFFICIAL SITE.</h1>
+                <p className='absolute top-0 left-0 w-full h-full flex justify-center items-center text-center text-4xl font-bold text-[#faebd7]'>We Aim to: </p>
+                {/* <Typed
+                    className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
+                    strings={["PLAY", "FIT", "SUCCESS"]}
+                    typeSpeed={115}
+                    backSpeed={135}
+                    loop
+                /> */}
             </div>
-            <h1 className='name'>WELCOME TO CHOPAL CRICKET ASSOCIATION OFFICIAL SITE</h1>
             <div>
             </div>
         </div>
-    )
+    );
 }
--- chopal/src/components/Navbar.js
+++ chopal/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import cpl from './images/cpl.jpg'
+import cpl from './images/cpl.jpg';
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
@@ -8,10 +8,11 @@ export default function Navbar() {
     const handleNav = () => {
         setNav(!nav);
     }
+
     return (
-        <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-            <img src={cpl} height='75px' width='75px' className='rounded-full' />
-            <h1 className="w-full text-3xl font-bold text-[#00df9a] ml-8">CHOAPL PREMIER LEAGUE</h1>
+        <div className="relative z-50 flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+            <img src={cpl} height='75px' width='75px' className='rounded-full sm:h-14 sm:w-14 md:h-18 md:w-18 lg:h-22 lg:w-22 xl:h-24 xl:w-24' alt='img' />
+            <h1 className="w-full text-2xl font-bold text-[#00df9a] sm:ml-6 ml-3 sm:text-md md:text-2xl lg:text-4xl xl:text-3xl">CHOPAL PREMIER LEAGUE</h1>
             <ul className="hidden md:flex">
                 <li className="p-4">Home</li>
                 <li className="p-4">Company</li>
@@ -24,7 +25,7 @@ export default function Navbar() {
                 {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             </div>
 
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 z-50' : 'fixed left-[-100%]'}>
                 <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">CPL</h1>
                 <ul className="uppercase p-4">
                     <li className="p-4 border-b border-gray-600">Home</li>
@@ -35,5 +36,5 @@ export default function Navbar() {
                 </ul>
             </div>
         </div>
-    )
+    );
 }
